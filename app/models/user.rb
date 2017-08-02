f17dcbfe93a2ec920e95f20063c439ed1d86dad1
KYEONGMIN CHO <ggingmin@likelion.org>
2017-08-02 01:55:21 +0000

    commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,12 +3,31 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable,
+         :omniauthable, :omniauth_providers => [:facebook]
+         
   validates :phone_number, presence: true, uniqueness: true, length: { is: 11 }  
   validates :address, presence: true
   validates :birth_date, presence: true
+  
   has_many :vets
   has_many :reviews
   has_many :reservations
+  
+  def self.new_with_session(params, session)
+    super.tap do |user|
+      if data = session["devise.facebook_data"] && session["devise.facebook_data"]["extra"]["raw_info"]
+        user.email = data["email"] if user.email.blank?
+      end
+    end
+  end
+
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+    user.email = auth.info.email
+    user.password = Devise.friendly_token[0,20]
+    user.name = auth.info.name
+    end
+  end  
   
 end
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -274,4 +274,5 @@ Devise.setup do |config|
   # When using OmniAuth, Devise cannot automatically set OmniAuth path,
   # so you need to do it manually. For the users scope, it would be:
   # config.omniauth_path_prefix = '/my_engine/users/auth'
+  config.omniauth :facebook, ENV["FB_APP_ID"], ENV["FB_APP_SECRET"], callback_url: "https://doctor-teddy-ggingmin.c9users.io:8080/users/auth/facebook/callback"  
 end
